03b4a133f9c8c89f6f77319627f8c701412c0868
wbluke <pwb0127@naver.com>
2019-01-02 10:31:37 +0900

    update 3_if.py

---
+++ 3_if.py
@@ -0,0 +1,15 @@
+# if_elif_else
+x = 10
+if x == 5:
+    print('x = 5')
+elif x == 10:
+    print('x = 10')
+else:
+    print('x = ?')
+
+
+# if you want to pass the 'if' block
+if x == 5:
+    pass # TODO : must fill this block later
+else:
+    print('x = 10')
